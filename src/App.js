312b41593dda855a0764d64617207e33928f32e6
RiouC <riou.christophe@protonmail.com>
2021-05-17 17:14:21 +0200

    hide _redirects; change NavLink styles

--- src/App.js
+++ src/App.js
@@ -1,88 +1,119 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    NavLink,
+    Redirect,
+    useRouteMatch,
+    useParams,
 } from "react-router-dom";
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
+    return (
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <NavLink to="/"
+        /* activeClassName="selected" */
+                         activeStyle={{
+                             fontWeight: "bold",
+                             color: "red"
+                         }}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about"
+                         activeStyle={{
+                             fontWeight: "bold",
+                             color: "red"
+                         }}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/topics"
+                         activeStyle={{
+                             fontWeight: "bold",
+                             color: "red"
+                         }}
+                >
+                  Topics
+                </NavLink>
+              </li>
+            </ul>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/topics">
+                <Topics />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+              <Route >
+                <Redirect to="/404"/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+    );
 }
 
 function Home() {
-  return <h2>Home</h2>;
+    return <h2>Home</h2>;
 }
 
 function About() {
-  return <h2>About</h2>;
+    return <h2>About</h2>;
 }
 
+const NotFound = () => {
+    return <h2>404 Not Found</h2>;
+};
+
 function Topics() {
-  let match = useRouteMatch();
+    let match = useRouteMatch();
 
-  return (
-    <div>
-      <h2>Topics</h2>
+    return (
+        <div>
+          <h2>Topics</h2>
 
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>
-            Props v. State
-          </Link>
-        </li>
-      </ul>
+          <ul>
+            <li>
+              <NavLink to={`${match.url}/components`}>Components</NavLink>
+            </li>
+            <li>
+              <NavLink to={`${match.url}/props-v-state`}>
+                Props v. State
+              </NavLink>
+            </li>
+          </ul>
 
-      {/* The Topics page has its own <Switch> with more routes
-          that build on the /topics URL path. You can think of the
-          2nd <Route> here as an "index" page for all topics, or
-          the page that is shown when no topic is selected */}
-      <Switch>
-        <Route path={`${match.path}/:topicId`}>
-          <Topic />
-        </Route>
-        <Route path={match.path}>
-          <h3>Please select a topic.</h3>
-        </Route>
-      </Switch>
-    </div>
-  );
+          {/* The Topics page has its own <Switch> with more routes
+             that build on the /topics URL path. You can think of the
+             2nd <Route> here as an "index" page for all topics, or
+             the page that is shown when no topic is selected */}
+          <Switch>
+            <Route path={`${match.path}/:topicId`}>
+              <Topic />
+            </Route>
+            <Route path={match.path}>
+              <h3>Please select a topic.</h3>
+            </Route>
+          </Switch>
+        </div>
+    );
 }
 
 function Topic() {
-  let { topicId } = useParams();
-  return <h3>Requested topic ID: {topicId}</h3>;
+    let { topicId } = useParams();
+    return <h3>Requested topic ID: {topicId}</h3>;
 }
